eb4e30e1e155cd31101c3f01bff0463afb75b46f
fabio-ericle <fabioericle@gmail.com>
2022-01-06 23:15:54 -0300

    update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,10 @@
 import "express-async-errors";
 import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import "./database";
 import routes from './routes';
+import { globalErrors } from "./middlewares/globalErrors";
 
 dotenv.config();
 const port = parseInt(process.env.PORT as string) || 3001;
@@ -11,17 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-   if(err instanceof Error) {
-      return response.status(400).json({
-         error: err.message
-      });
-   }
-   return response.status(500).json({
-      status: "error",
-      message: "Internal Server Error!",
-   });
-});
+app.use(globalErrors);
 app.listen(port, () => {
    console.log("Server is running!");
 });
