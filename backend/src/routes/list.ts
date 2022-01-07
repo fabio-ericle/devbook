f595d1825034322a388a782468ce984a7559d778
fabio-ericle <fabioericle@gmail.com>
2022-01-07 08:54:10 -0300

    Update list.ts

--- backend/src/routes/list.ts
+++ backend/src/routes/list.ts
@@ -10,7 +10,8 @@ const postController = new PostController();
 
 list.get("/users", ensureAuthenticated, userController.list);
 list.get("/user", ensureAuthenticated, userController.user);
+list.get("/user/:user_id", ensureAuthenticated, userController.userById);
 list.get("/posts", ensureAuthenticated, postController.get);
-list.get("/post", ensureAuthenticated, postController.getById);
+list.get("/post/:id", ensureAuthenticated, postController.getById);
 
 export { list };
