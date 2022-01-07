856edee6088a8f9512059aa69504599894e411aa
fabio-ericle <fabioericle@gmail.com>
2022-01-06 23:22:29 -0300

    Update ensureAuthenticated.ts

--- backend/src/middlewares/ensureAuthenticated.ts
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -10,10 +10,13 @@ export function ensureAuthenticated(request: Request, response: Response, next: 
    if (!authToken) {
       return response.status(401).json({"status" : "Unauthorized"}).end();
    }
+
    const [, token] = authToken.split(" ");
+   
    try {
       const { sub } = verify(token, process.env.SESSION_TOKEN as string) as IPayload;
       request.user_id = sub;
+
       return next();
    } catch (error) {
       return response.status(401).json({"status" : "Unauthorized"}).end();
