d95b115edca49e390dd6ee796c2d286ea0d9d4b2
fabio-ericle <fabioericle@gmail.com>
2022-01-07 08:53:24 -0300

    Update UserController.ts

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -19,7 +19,15 @@ export class UserController {
 
    async user(request: Request, response: Response) {
       const userServices = new UserServices();
-      const user = await userServices.getById(request.user_id);
+      const user = await userServices.getUser(request.user);
+      
+      return response.json(user);
+   }
+
+   async userById(request: Request, response: Response) {
+      const userServices = new UserServices();
+      const { user_id = '' } = request.params;
+      const user = await userServices.getById({ user_id });
       
       return response.json(user);
    }
@@ -34,15 +42,14 @@ export class UserController {
 
    async delete(request: Request, response: Response) {
       const userServices = new UserServices();
-      const { user_id = '' } = request.body;
-      const result = await userServices.delete(user_id);
+      const result = await userServices.delete(request.user);
 
       return response.status(200).send(result);
    }
 
    async auth(request: Request, response: Response) {
-      const { user_email, user_password } = request.body;
       const userServices = new UserServices();
+      const { user_email = '', user_password = ''} = request.body;
       const result = await userServices.auth({ user_email, user_password });
       
       return response.status(200).send(result);
