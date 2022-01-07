5d71edddcb32904cac4be392cd7821673b48393c
fabio-ericle <fabioericle@gmail.com>
2022-01-06 23:21:55 -0300

    Update UserController.ts

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -3,42 +3,48 @@ import { UserServices } from '../services/UserServices';
 
 export class UserController {
    async create(request: Request, response: Response) {
-      const { user_name, user_email, user_password } = request.body;
       const userServices = new UserServices();
-      const result = await userServices.create({ data: { user_name, user_email, user_password } });
-      return response.json(result);
+      const { user_name = '', user_email = '', user_password = ''} = request.body;
+      const result = await userServices.create({ user_name, user_email, user_password });
+
+      return response.status(201).send(result);
    }
 
    async list(request: Request, response: Response) {
       const userService = new UserServices();
       const result = await userService.get();
-      return response.json(result);
+
+      return response.status(200).send(result);
    }
 
    async user(request: Request, response: Response) {
       const userServices = new UserServices();
       const user = await userServices.getById(request.user_id);
+      
       return response.json(user);
    }
 
    async update(request: Request, response: Response) {
       const { user_id, user_name, user_email } = request.body;
       const userServices = new UserServices();
-      const result = await userServices.update({ data: { user_id, user_name, user_email } });
-      return response.json(result);
+      const result = await userServices.update({ user_id, user_name, user_email });
+
+      return response.status(200).send(result);
    }
 
    async delete(request: Request, response: Response) {
-      const { user_id } = request.body;
       const userServices = new UserServices();
+      const { user_id = '' } = request.body;
       const result = await userServices.delete(user_id);
-      return response.json(result);
+
+      return response.status(200).send(result);
    }
 
    async auth(request: Request, response: Response) {
       const { user_email, user_password } = request.body;
       const userServices = new UserServices();
-      const result = await userServices.auth({ data: { user_email, user_password } });
-      return response.json(result);
+      const result = await userServices.auth({ user_email, user_password });
+      
+      return response.status(200).send(result);
    }
 }
