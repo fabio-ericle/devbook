5679efe55026e18890daf84d7b82c0dd61685e06
fabio-ericle <fabioericle@gmail.com>
2022-01-06 23:23:56 -0300

    Update PostServices.ts

--- backend/src/services/PostServices.ts
+++ backend/src/services/PostServices.ts
@@ -2,6 +2,8 @@ import { getCustomRepository } from 'typeorm';
 import { PostRepository } from '../repositories/PostRepository';
 import { UserRepository } from '../repositories/UserRepository';
 import { classToPlain } from 'class-transformer';
+import { Post } from '../entities/Post';
+import { User } from '../entities/User';
 
 type PostProps = {
    data: {
@@ -12,31 +14,41 @@ type PostProps = {
 }
 
 export class PostServices {
-   async create({ data }: PostProps) {
+   async create(post: Partial<Post>, user: Partial<User>) {
       const postRepository = getCustomRepository(PostRepository);
       const userRepository = getCustomRepository(UserRepository);
-      if (data.post_title.length < 5 || data.post_content.length < 5 || data.user_id.length < 5) {
+
+      if (post.post_title.length < 5 || post.post_content.length < 5 || post.user_id.length < 5) {
          throw new Error("Não foi possível concluir a ação!");
       }
-      const user = await userRepository.findOne({ "user_id": data.user_id });
+
+      const userExists = await userRepository.findOne(
+         { where: { user_id : user.user_id } }
+      );
       if (!user) {
          throw new Error("Não foi possível concluir a ação!");
       }
+
       const dateNow = Date.now();
       const date = new Date(dateNow);
-      const post = postRepository.create({
-         post_title: data.post_title,
-         post_content: data.post_content,
-         user_id: data.user_id,
+
+      const createPost = postRepository.create({
+         post_title: post.post_title,
+         post_content: post.post_content,
+         user_id: post.user_id,
          created_at: date,
       });
-      await postRepository.save(post);
+
+      await postRepository.save(createPost);
+      
       return { "status": "concluido!" };
    }
 
    async get() {
       const postRepository = getCustomRepository(PostRepository);
+      
       const posts = await postRepository.find();
+
       return classToPlain(posts);
    }
 
@@ -44,11 +56,16 @@ export class PostServices {
       if (id.length < 10 || id === '') {
          throw new Error("Impossível concluir a ação!");
       }
+
       const postRepository = getCustomRepository(PostRepository);
-      const post = await postRepository.findOne({ "post_id": id });
+      
+      const post = await postRepository.findOne({ 
+         where: { post_id : id }
+       });
       if (!post) {
          throw new Error("Publicação não encontrada!");
       }
+
       return classToPlain(post);
    }
 
@@ -56,14 +73,20 @@ export class PostServices {
       if (id.length < 10 || id === '') {
          throw new Error("Impossível concluir a ação!");
       }
+
       const postRepository = getCustomRepository(PostRepository);
-      const post = await postRepository.findOne({ "post_id": id });
+
+      const post = await postRepository.findOne({
+         where: { post_id : id }
+      });
       if (!post) {
          throw new Error("Publicação não encontrada!");
       }
+
       await postRepository.delete({
          "post_id" : id
       });
+      
       return {"status" : "concluído"};
    }
 }
